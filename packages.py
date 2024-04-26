92883d9fe27b347cb652dd661fe3ef7e986a02d7
lichtbrenger <michaelgeuensbe@gmail.com>
2024-04-26 17:45:36 +0200

    added creating a database

--- packages.py
+++ packages.py
@@ -8,8 +8,52 @@ import re
 import os
 import sqlite3
 
-def create_package_database():
-   os.system('cp /var/lib/dnf/history.sqlite ./packages.sqlite') 
+'''
+This function retrieves the Operating System that is being used.
+'''
+def retrieve_operating_system():
+    if subprocess.call(['which', 'apt-get']) == 0:
+        return 'ubuntu'
+    if subprocess.call(['which', 'yum']) == 0:
+        return 'fedora'
+
+'''
+Fetch rows from database
+'''
+def retrieve_rows():
+    operating_system = retrieve_operating_system()
+    if operating_system == 'fedora':
+        conn = sqlite3.connect('/var/lib/dnf/history.sqlite')
+        cursor = conn.cursor()
+        cursor.execute("SELECT name,version FROM rpm")
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+
+'''
+Create a local copy of the existing database
+'''
+def create_personal_database():
+    filename = './packages.sqlite'
+    # Check if the file exists
+    if not os.path.exists('./packages.sqlite'):
+         # Command to create a file
+        command = f"touch {filename}"
+        # Execute the command using subprocess.Popen
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(f"File '{filename}' created successfully.")
+    else:
+        print(f"File '{filename}' already exists.")
+
+    operating_system = retrieve_operating_system()
+    if operating_system == 'fedora':
+        conn_dest = sqlite3.connect('./packages.sqlite')
+        cursor_dest = conn_dest.cursor()
+        cursor_dest.execute('''CREATE TABLE IF NOT EXISTS packages
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, version TEXT, vulnerable INTEGER)''')
+        for row in rows:
+            cursor_dest.execute("INSERT INTO packages (name, version) VALUES (?, ?)", (row[1], row[2]))
 
 
 '''
@@ -80,4 +124,4 @@ def get_installed_packages():
     else:
         return "Unsupported package manager."
 
-
+retrieve_rows()
