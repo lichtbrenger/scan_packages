96c0d675bf8c58ce35ff368334fabc5ea920fd0e
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-03 14:29:56 +0200

    discard uninstalled packages logic

--- database.py
+++ database.py
@@ -93,9 +93,43 @@ def add_newly_installed_packages():
 
         conn.commit()
         conn.close()
+def remove_package_from_db(package_name):
+    conn = sqlite3.connect('./packages.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+                    DELETE FROM packages
+                    WHERE name = ?;
+                    ''', (package_name))
+    conn.commit()
+    conn.close()
+
+def retrieve_db_packages_name():
+    conn = sqlite3.connect('./packages.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM packages")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def retrieve_packages_name():
+    operating_system = retrieve_operating_system()
+    if operating_system == 'fedora':
+        conn = sqlite3.connect('/var/lib/dnf/history.sqlite')
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM rpm")
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+
 
 def discard_uninstalled_packages():
-    return 'not implemented yet'
+    os_packages = set(retrieve_packages_name())
+    database_packages = set(retrieve_db_packages_name())
+    uninstalled_packages = database_packages - os_packages
+    uninstalled_packages = list(uninstalled_packages)
+    for uninstalled_package in uninstalled_packages:
+        remove_package_from_db(uninstalled_package)
 
 def update_package(package):
     conn = sqlite3.connect('./packages.sqlite')
@@ -119,4 +153,4 @@ def update_packages():
                 if db_package[2] != os_package[1]:
                     update_package((db_package[0], os_package[1]))
 
-
+discard_uninstalled_packages()
