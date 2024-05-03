d64acc8525c9499d30f49c73bac57482de86e9bb
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-03 13:15:09 +0200

    added logic for updating database packages

--- database.py
+++ database.py
@@ -66,3 +66,57 @@ main process, creates a local copy of the existing database
 def create_local_database():
     create_file()
     insert_packages()
+
+# create_local_database()
+
+def retrieve_db_packages():
+    conn = sqlite3.connect('./packages.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("SELECT id,name,version FROM packages")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+
+
+def add_newly_installed_packages():
+    os_packages = set(retrieve_packages())
+    database_packages = set(retrieve_db_packages())
+    new_packages = database_packages.difference(os_packages)
+    if new_packages:
+        # insert new packages into local sqlite db
+        conn = sqlite3.connect('./packages.sqlite')
+        cursor = conn.cursor()
+
+        for package in new_packages:
+            cursor.execute("INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)", (package[0], package[1], 2))
+
+        conn.commit()
+        conn.close()
+
+def discard_uninstalled_packages():
+    return 'not implemented yet'
+
+def update_package(package):
+    conn = sqlite3.connect('./packages.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+            UPDATE packages
+            SET version = ?, vulnerable = ?
+            WHERE id = ?;
+            ''', (package[0], 2, package[1]))
+
+    conn.commit()
+    conn.close()
+
+def update_packages():
+    os_packages = retrieve_packages()
+    db_packages = retrieve_db_packages()
+    for db_package in db_packages:
+        for os_package in os_packages:
+            if db_package[1] == os_package[0]:
+                if db_package[2] != os_package[1]:
+                    update_package((db_package[0], os_package[1]))
+
+
