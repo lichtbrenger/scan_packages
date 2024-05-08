6e43dc40416a1d1373a64277770936a7e85297d4
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-08 16:51:52 +0200

    annotation and removal of check all packages function

--- scan.py
+++ scan.py
@@ -14,12 +14,11 @@ using a local version of the national vulnerability database.
 We try to find a match with the CPE id by using seperators (:) together with the package
 name and version.
 e.g. cpe:2.3:a:zlib:zlib:1.1.4:*:*:*:*:*:*:*
-Because sqlite does not support booleans we use integers.
+Because sqlite does not support booleans we use integers to represent the state of the package
 2 = not scanned yet
 1 = not vulnerable 
 0 = vulnerable
 '''
-
 def check_vulnerabilities_online(package):
     url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={package[1]}"
     response = requests.get(url)
@@ -36,10 +35,18 @@ def check_vulnerabilities_online(package):
 
     return tuple(package)
 
+'''
+displays the currently vulnerable packages from the personal database
+'''
 def display_vulnerable_packages():
     vulnerable_packages = packages.retrieve_vulnerable_packages()
     print(vulnerable_packages)
-    
+   
+'''
+Retrieves an unchecked package from the personal database.
+Then checks that package for vulnerabilities.
+It updates the status of the package to either vulnerable or not vulnerable.
+'''
 def check_package():
     installed_package = packages.retrieve_package()
     if not installed_package:
@@ -48,19 +55,5 @@ def check_package():
     packages.update_package(checked_package)
     return True
 
-def check_all_packages():
-    installed_packages = packages.get_installed_packages()
-    amount_of_vulnerabilities = 0
-    for package in installed_packages:
-        contains_vulnerability = check_vulnerabilities(package)
-        if contains_vulnerability:
-            amount_of_vulnerabilities += 1
-
-    if amount_of_vulnerabilities > 0:
-        return True
-
-    return False
-
-#while True:
-    #check_package()
-check_package()
+while True:
+    check_package()
