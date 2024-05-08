b8a14a7680bc12746bef169d4fe1b2632b5a3a68
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-08 13:27:11 +0200

    separating logic between database and one time setup

--- database.py
+++ database.py
@@ -8,67 +8,6 @@ import re
 import os
 import sqlite3
 
-'''
-This function retrieves the Operating System that is being used.
-'''
-def retrieve_operating_system():
-    if subprocess.call(['which', 'apt-get']) == 0:
-        return 'ubuntu'
-    if subprocess.call(['which', 'yum']) == 0:
-        return 'fedora'
-
-'''
-Fetch rows from database
-'''
-def retrieve_packages():
-    operating_system = retrieve_operating_system()
-    if operating_system == 'fedora':
-        conn = sqlite3.connect('/var/lib/dnf/history.sqlite')
-        cursor = conn.cursor()
-        cursor.execute("SELECT name,version FROM rpm")
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
-
-def create_file():
-    filename = './packages.sqlite'
-    # Check if the file exists
-    if not os.path.exists('./packages.sqlite'):
-         # Command to create a file
-        command = f"touch {filename}"
-        # Execute the command using subprocess.Popen
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        print(f"File '{filename}' created successfully.")
-    else:
-        print(f"File '{filename}' already exists.")
-
-'''
-creates a table in the local database and inserts 
-the installed packages
-'''
-def insert_packages():
-    conn = sqlite3.connect('./packages.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS packages
-                  (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, version TEXT, vulnerable INTEGER)''')
-
-    packages = retrieve_packages()
-    for package in packages:
-        cursor.execute("INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)", (package[0], package[1], 2))
-
-    conn.commit()
-    conn.close()
-
-'''
-main process, creates a local copy of the existing database
-'''
-def create_local_database():
-    create_file()
-    insert_packages()
-
-# create_local_database()
-
 def retrieve_db_packages():
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
@@ -76,8 +15,6 @@ def retrieve_db_packages():
     rows = cursor.fetchall()
     conn.close()
     return rows
-
-
 
 def add_newly_installed_packages():
     os_packages = set(retrieve_packages())
@@ -93,6 +30,7 @@ def add_newly_installed_packages():
 
         conn.commit()
         conn.close()
+
 def remove_package_from_db(package_name):
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
@@ -152,5 +90,3 @@ def update_packages():
             if db_package[1] == os_package[0]:
                 if db_package[2] != os_package[1]:
                     update_package((db_package[0], os_package[1]))
-
-discard_uninstalled_packages()
---
+++ setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+'''
+This module facalitates the creating of an environment necessary for the app to function.
+It needs to be run first in order for the app to work
+'''
+import subprocess
+import re
+import os
+import sqlite3
+
+'''
+This function retrieves the Operating System that is being used.
+'''
+def retrieve_operating_system():
+    if subprocess.call(['which', 'apt-get']) == 0:
+        return 'ubuntu'
+    if subprocess.call(['which', 'yum']) == 0:
+        return 'fedora'
+
+'''
+This function retrieves the rows of the operating system's database.
+This is used to create our own database where we can add properties to
+packages without altering the base system.
+This also ensures easy removal
+'''
+def retrieve_packages():
+    operating_system = retrieve_operating_system()
+    if operating_system == 'fedora':
+        conn = sqlite3.connect('/var/lib/dnf/history.sqlite')
+        cursor = conn.cursor()
+        cursor.execute("SELECT name,version FROM rpm")
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+
+'''
+Create the personal database where operations will be run on.
+The personal database is a copy of the OS database for packages including 
+certain properties related to vulnerabilities.
+'''
+def create_file():
+    filename = './packages.sqlite'
+    # Check if the file exists
+    if not os.path.exists('./packages.sqlite'):
+         # Command to create a file
+        command = f"touch {filename}"
+        # Execute the command using subprocess.Popen
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(f"File '{filename}' created successfully.")
+    else:
+        print(f"File '{filename}' already exists.")
+
+'''
+creates a table in the personal database and inserts 
+the installed packages that are found in the OS's database including the vulnerability status.
+'''
+def insert_packages():
+    conn = sqlite3.connect('./packages.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS packages
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, version TEXT, vulnerable INTEGER)''')
+
+    packages = retrieve_packages()
+    for package in packages:
+        cursor.execute("INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)", (package[0], package[1], 2))
+
+    conn.commit()
+    conn.close()
+
+'''
+starts the setup
+'''
+def create_local_database():
+    create_file()
+    insert_packages()
+
+create_local_database()
+
+
