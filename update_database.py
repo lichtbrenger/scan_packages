340edadabbb684392812206325ab0c7ffeb701d4
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-10 18:35:46 +0200

    improved separation of concerns, added comments, removed duplicate functions

--- update_database.py
+++ update_database.py
@@ -8,92 +8,48 @@ import re
 import os
 import logging
 import sqlite3
+import database
+import os_interface
 logger = logging.getLogger(__name__)
 
-def retrieve_packages():
-    operating_system = retrieve_operating_system()
-    if operating_system == 'fedora':
-        conn = sqlite3.connect('/var/lib/dnf/history.sqlite')
-        cursor = conn.cursor()
-        cursor.execute("SELECT name,version FROM rpm")
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
-
-def retrieve_db_packages():
-    conn = sqlite3.connect('./packages.sqlite')
-    cursor = conn.cursor()
-    cursor.execute("SELECT id,name,version FROM packages")
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
-
+'''
+Adds newly installed packages to the personal database.
+We use Set theory by calculating the difference between the
+set of packages from the personal database and the set of packages
+from the operating system's database.
+'''
 def add_newly_installed_packages():
-    os_packages = set(retrieve_packages())
-    database_packages = set(retrieve_db_packages())
+    os_packages = set(os_interface.retrieve_packages())
+    database_packages = set(database.retrieve_packages())
     new_packages = database_packages.difference(os_packages)
     if new_packages:
-        # insert new packages into local sqlite db
-        conn = sqlite3.connect('./packages.sqlite')
-        cursor = conn.cursor()
-
         for package in new_packages:
-            cursor.execute("INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)", (package[0], package[1], 2))
+            database.insert_package(package)
 
-        conn.commit()
-        conn.close()
-
-def remove_package_from_db(package_name):
-    conn = sqlite3.connect('./packages.sqlite')
-    cursor = conn.cursor()
-
-    cursor.execute('''
-                    DELETE FROM packages
-                    WHERE name = ?;
-                    ''', (package_name))
-    conn.commit()
-    conn.close()
-
-def retrieve_db_packages_name():
-    conn = sqlite3.connect('./packages.sqlite')
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM packages")
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
-
-def retrieve_packages_name():
-    operating_system = retrieve_operating_system()
-    if operating_system == 'fedora':
-        conn = sqlite3.connect('/var/lib/dnf/history.sqlite')
-        cursor = conn.cursor()
-        cursor.execute("SELECT name FROM rpm")
-        rows = cursor.fetchall()
-        conn.close()
-        return rows
-
-
+'''
+Compares the operating system database
+and the personal database.
+When packages that are in the personal database are
+not found in the operating system's database we remove that package
+from the personal database.
+'''
 def discard_uninstalled_packages():
-    os_packages = set(retrieve_packages_name())
-    database_packages = set(retrieve_db_packages_name())
+    os_packages = set(os_interface.retrieve_packages_name())
+    database_packages = set(database.retrieve_db_packages_name())
     uninstalled_packages = database_packages - os_packages
     uninstalled_packages = list(uninstalled_packages)
     for uninstalled_package in uninstalled_packages:
-        remove_package_from_db(uninstalled_package)
+        database.remove_package_from_db(uninstalled_package)
 
-def update_package(package):
-    conn = sqlite3.connect('./packages.sqlite')
-    cursor = conn.cursor()
-
-    cursor.execute('''
-            UPDATE packages
-            SET version = ?, vulnerable = ?
-            WHERE id = ?;
-            ''', (package[0], 2, package[1]))
-
-    conn.commit()
-    conn.close()
-
+'''
+Updates all packages using the update package function.
+It retrieves all packages that are listed in the operating system and,
+the personal database.
+Every package is compared by name.
+When a match is found, the version of the package is compared.
+When there is a discrepancy we assume the package version is outdated because the only change in version
+could come an update in the operating system's database.
+'''
 def update_packages():
     os_packages = retrieve_packages()
     db_packages = retrieve_db_packages()
@@ -101,6 +57,6 @@ def update_packages():
         for os_package in os_packages:
             if db_package[1] == os_package[0]:
                 if db_package[2] != os_package[1]:
-                    update_package((db_package[0], os_package[1]))
+                    database.update_package((db_package[0], os_package[1]))
 
 
