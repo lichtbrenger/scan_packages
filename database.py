f775185fa8550eaa867266c9d758e1ef8e88bdf0
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-12 15:36:16 +0200

    flake8 compliant

--- database.py
+++ database.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
-'''
-This module handles operations on the loca database.
-'''
-
-import subprocess
-import re
-import os
 import logging
 import sqlite3
 import os_interface
 logger = logging.getLogger(__name__)
 
 '''
-retrieve all data from the packages in the 
+retrieve all data from the packages in the
 personal database
 '''
+
+
 def retrieve_packages():
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
@@ -24,17 +19,20 @@ def retrieve_packages():
     logger.info('successfully retrieved all packages from personal database\n')
     return rows
 
+
 def insert_package(package):
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
-
-    cursor.execute("INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)", (package[0], package[1], 2))
+    query = "INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)"
+    cursor.execute(query, (package[0], package[1], 2))
 
     conn.commit()
     conn.close()
-    logger.info(f'successfully added package {package} to the personal database.')
+    logger.info('successfully added package'
+                '{} to the personal database.'
+                .format(package)
+                )
 
-    
 
 def remove_package_from_db(package_name):
     try:
@@ -48,15 +46,18 @@ def remove_package_from_db(package_name):
         conn.commit()
         conn.close()
         logger.info(f'deleted {package_name} from personal database.')
-        
+
     except sqlite3.Error as e:
         logger.error(f'The following error was encountered:\n{e}.')
+
 
 '''
 Retrieves the all the names of the packages
 from the personal database.
 and returns a list of tupples.
 '''
+
+
 def retrieve_packages_name():
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
@@ -66,38 +67,14 @@ def retrieve_packages_name():
     logger.info('successfully retrieved package names from personal database.')
     return rows
 
+
 '''
 Updates one package given an ID.
 The function takes a package and,
 resets the vulnerability status to unchecked.
 '''
-def update_package(package):
-    conn = sqlite3.connect('./packages.sqlite')
-    cursor = conn.cursor()
-
-    cursor.execute('''
-            UPDATE packages
-            SET version = ?, vulnerable = ?
-            WHERE id = ?;
-            ''', (package[0], 2, package[1]))
-
-    conn.commit()
-    conn.close()
-
-def update_packages():
-    os_packages = retrieve_packages()
-    db_packages = os_interface.retrieve_packages()
-    for db_package in db_packages:
-        for os_package in os_packages:
-            if db_package[1] == os_package[0]:
-                if db_package[2] != os_package[1]:
-                    update_package((db_package[0], os_package[1]))
 
 
-
-'''
-Updates vulnerable packages using a local sqlite database.
-'''
 def update_package(package):
     if package:
         logger.info('updating package:' + package[1])
@@ -114,6 +91,17 @@ def update_package(package):
 
     logger.info(f'package, {package}, could not be updated.')
 
+
+def update_packages():
+    os_packages = retrieve_packages()
+    db_packages = os_interface.retrieve_packages()
+    for db_package in db_packages:
+        for os_package in os_packages:
+            if db_package[1] == os_package[0]:
+                if db_package[2] != os_package[1]:
+                    update_package((db_package[0], os_package[1]))
+
+
 def retrieve_package():
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
@@ -129,9 +117,10 @@ def retrieve_package():
     if not package:
         logger.info('All packages scanned')
         return
-    
+
     logger.info('retrieved package: ' + package[1])
     return package
+
 
 def retrieve_vulnerable_packages():
     conn = sqlite3.connect('./packages.sqlite')
@@ -148,6 +137,7 @@ def retrieve_vulnerable_packages():
 
     return vulnerable_packages
 
+
 def count_vulnerable_packages():
     conn = sqlite3.connect('/home/yide/scan_packages/packages.sqlite')
     cursor = conn.cursor()
@@ -162,4 +152,3 @@ def count_vulnerable_packages():
         logger.info('No vulnerable packages found')
 
     return vulnerable_packages[0][0]
-
--- scan.py
+++ scan.py
@@ -2,8 +2,7 @@
 '''
 This module scans installed packages
 '''
-
-import os
+import logging
 import database
 import re
 import requests
@@ -13,20 +12,26 @@ logger = logging.getLogger(__name__)
 '''
 Checks vulnerabilities of the given package
 using a local version of the national vulnerability database.
-We try to find a match with the CPE id by using seperators (:) together with the package
-name and version.
+We try to find a match with the CPE id by using seperators (:)
+together with the package name and version.
 e.g. cpe:2.3:a:zlib:zlib:1.1.4:*:*:*:*:*:*:*
-Because sqlite does not support booleans we use integers to represent the state of the package
+Because sqlite does not support booleans we use integers to represent
+the state of the package
 2 = not scanned yet
-1 = not vulnerable 
+1 = not vulnerable
 0 = vulnerable
 '''
+
+
 def check_vulnerabilities_online(package):
     package = list(package)
-    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={package[1]}"
+    url = ('https://services.nvd.nist.gov/rest/json'
+           '/cves/2.0?keywordSearch={}'
+           .format(package[1])
+           )
     response = requests.get(url)
     if not response.status_code == 200:
-       logger.error('NIST API could not be queried.')
+        logger.error('NIST API could not be queried.')
 
     response_data = response.json()
     if response_data['totalResults'] == 0:
@@ -45,12 +50,14 @@ def check_vulnerabilities_online(package):
     package[3] = 1
     return tuple(package)
 
-  
+
 '''
 Retrieves an unchecked package from the personal database.
 Then checks that package for vulnerabilities.
 It updates the status of the package to either vulnerable or not vulnerable.
 '''
+
+
 def check_package():
     installed_package = database.retrieve_package()
     if not installed_package:
