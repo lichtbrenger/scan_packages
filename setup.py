bdfb97ff4160d5db188acf847e66b14c4e40aed6
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-14 18:22:15 +0200

    flake8 setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 '''
-This module facalitates the creating of an environment necessary for the app to function.
+This module facalitates the creating of an
+environment necessary for the app to function.
 It needs to be run first in order for the app to work
 '''
+
+
 import subprocess
-import re
 import os
 import sqlite3
 
-def create_file():
+
+def create_log():
     filename = './daemon.log'
     # Check if the file exists
     if not os.path.exists('./daemon.log'):
-         # Command to create a file
+        # Command to create a file
         command = f"touch {filename}"
         # Execute the command using subprocess.Popen
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(command,
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         print(f"File '{filename}' created successfully.")
     else:
@@ -25,11 +31,14 @@ def create_file():
 '''
 This function retrieves the Operating System that is being used.
 '''
+
+
 def retrieve_operating_system():
     if subprocess.call(['which', 'apt-get']) == 0:
         return 'ubuntu'
     if subprocess.call(['which', 'yum']) == 0:
         return 'fedora'
+
 
 '''
 This function retrieves the rows of the operating system's database.
@@ -37,6 +46,8 @@ This is used to create our own database where we can add properties to
 packages without altering the base system.
 This also ensures easy removal
 '''
+
+
 def retrieve_packages():
     operating_system = retrieve_operating_system()
     if operating_system == 'fedora':
@@ -47,48 +58,67 @@ def retrieve_packages():
         conn.close()
         return rows
 
+
 '''
 Create the personal database where operations will be run on.
-The personal database is a copy of the OS database for packages including 
+The personal database is a copy of the OS database for packages including
 certain properties related to vulnerabilities.
 '''
-def create_file():
+
+
+def create_database():
     filename = './packages.sqlite'
     # Check if the file exists
     if not os.path.exists('./packages.sqlite'):
-         # Command to create a file
+        # Command to create a file
         command = f"touch {filename}"
         # Execute the command using subprocess.Popen
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(command,
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         print(f"File '{filename}' created successfully.")
     else:
         print(f"File '{filename}' already exists.")
 
+
 '''
-creates a table in the personal database and inserts 
-the installed packages that are found in the OS's database including the vulnerability status.
+creates a table in the personal database and inserts
+the installed packages that are found in the OS's
+database including the vulnerability status.
 '''
+
+
 def insert_packages():
     conn = sqlite3.connect('./packages.sqlite')
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS packages
-                  (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, version TEXT, vulnerable INTEGER)''')
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS packages
+                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT, version TEXT, vulnerable INTEGER)
+                   ''')
 
     packages = retrieve_packages()
     for package in packages:
-        cursor.execute("INSERT INTO packages (name, version, vulnerable) VALUES (?, ?, ?)", (package[0], package[1], 2))
+        cursor.execute('''
+                       INSERT INTO packages
+                       (name, version, vulnerable) VALUES (?, ?, ?)
+                       ''', (package[0], package[1], 2))
 
     conn.commit()
     conn.close()
 
+
 '''
 starts the setup
 '''
+
+
 def create_local_database():
-    create_file()
+    create_log()
+    create_database()
     insert_packages()
 
+
 create_local_database()
-
-
