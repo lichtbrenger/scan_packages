7fa7f4353eee5c86a83f15a9b9f24c9835e5e9fb
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-14 18:24:01 +0200

    flake8 update_database.py

--- update_database.py
+++ update_database.py
@@ -3,14 +3,11 @@
 This module handles operations on the loca database.
 '''
 
-import subprocess
-import re
-import os
 import logging
-import sqlite3
 import database
 import os_interface
 logger = logging.getLogger(__name__)
+
 
 '''
 Adds newly installed packages to the personal database.
@@ -18,6 +15,8 @@ We use Set theory by calculating the difference between the
 set of packages from the personal database and the set of packages
 from the operating system's database.
 '''
+
+
 def add_newly_installed_packages():
     os_packages = set(os_interface.retrieve_packages())
     database_packages = set(database.retrieve_packages())
@@ -26,6 +25,7 @@ def add_newly_installed_packages():
         for package in new_packages:
             database.insert_package(package)
 
+
 '''
 Compares the operating system database
 and the personal database.
@@ -33,6 +33,8 @@ When packages that are in the personal database are
 not found in the operating system's database we remove that package
 from the personal database.
 '''
+
+
 def discard_uninstalled_packages():
     os_packages = set(os_interface.retrieve_packages_name())
     database_packages = set(database.retrieve_packages_name())
@@ -41,15 +43,19 @@ def discard_uninstalled_packages():
     for uninstalled_package in uninstalled_packages:
         database.remove_package_from_db(uninstalled_package)
 
+
 '''
 Updates all packages using the update package function.
 It retrieves all packages that are listed in the operating system and,
 the personal database.
 Every package is compared by name.
 When a match is found, the version of the package is compared.
-When there is a discrepancy we assume the package version is outdated because the only change in version
+When there is a discrepancy we assume the package version is
+outdated because the only change in version
 could come an update in the operating system's database.
 '''
+
+
 def update_packages():
     os_packages = os_interface.retrieve_packages()
     db_packages = database.retrieve_packages()
@@ -58,5 +64,3 @@ def update_packages():
             if db_package[1] == os_package[0]:
                 if db_package[2] != os_package[1]:
                     database.update_package((db_package[0], os_package[1]))
-
-
