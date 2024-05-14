072c57f0a8286aac00c3db64c8c789c31b477d08
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-14 18:15:06 +0200

    flake8 compliant daemon.py

--- daemon.py
+++ daemon.py
@@ -1,18 +1,22 @@
-import os
-import sys
 import scan
 import logging
 import multiprocessing
 import threading
 import time
-import resource
+logger = logging.basicConfig(level=logging.INFO,
+                             filename='daemon.log',
+                             filemode='w',
+                             format='%(message)s')
 
 
 '''
 Define the work that each worker should be assigned.
 In this case each worker will check a package from the personal database
-continuously. When the scan did not complete successfully we stop the scanning process of that worker.
+continuously. When the scan did not complete successfully
+we stop the scanning process of that worker.
 '''
+
+
 def worker_function(task_id):
     while True:
         successful = scan.check_package()
@@ -22,13 +26,13 @@ def worker_function(task_id):
 
 
 '''
-Main function were we create a number of workers equivalnt to the number of CPU cores.
+Main function were we create a number of workers
+equivalent to the number of CPU cores.
 Afterward we will put the work to work asynchronously.
 '''
-def start_process():
-    # configure logging
-    logger = logging.basicConfig(level=logging.INFO, filename='daemon.log', filemode='w', format='%(message)s')
 
+
+def start_process():
     # Define the number of worker processes
     num_workers = multiprocessing.cpu_count()  # Use the number of CPU cores
 
@@ -42,5 +46,6 @@ def start_process():
 
     except KeyboardInterrupt:
         logger.info('program stopped')
+
 
 start_process()
