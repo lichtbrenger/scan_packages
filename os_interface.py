418faf41a281f75d30fdf8a5d7897d9fb3e3ccce
lichtbrenger <michaelgeuensbe@gmail.com>
2024-05-14 18:11:10 +0200

    flake8 os_interface.py

--- os_interface.py
+++ os_interface.py
@@ -1,6 +1,4 @@
 import subprocess
-import re
-import os
 import logging
 import sqlite3
 logger = logging.getLogger(__name__)
@@ -9,21 +7,25 @@ logger = logging.getLogger(__name__)
 Determines the operating system and return the local
 database that is used for storing data about packages
 '''
+
+
 def determine_os_database():
-    os_dict = { 'operating system': '', 'database location': '', 'queries': [] }
+    os_dict = {'operating system': '', 'database location': '', 'queries': []}
     if subprocess.call(['which', 'apt-get']) == 0:
         os_dict['operating system'] = 'ubuntu'
         os_dict['database location'] = '/var/lib/apt/lists/sources.sqlite3'
     if subprocess.call(['which', 'yum']) == 0:
         os_dict['operating system'] = 'fedora'
         os_dict['database location'] = '/var/lib/dnf/history.sqlite'
-        os_dict['queries'] = ['select name,version from rpm', 'select name from rpm']
+        os_dict['queries'] = ['select name,version from rpm',
+                              'select name from rpm']
 
-    if os_dict['operating system'] and os_dict['database location'] and os_dict['queries']:
-        logger.info(f'found operating system: {os_dict["operating system"]}')
-        return os_dict
+    if not os_dict['operating system']:
+        logger.error('Operating system and corresponding'
+                     'database could not be found.')
 
-    logger.error('Operating system and corresponding database could not be found.')
+    logger.info(f'found operating system: {os_dict["operating system"]}')
+    return os_dict
 
 
 def connect_to_db(database_location):
@@ -39,6 +41,7 @@ def connect_to_db(database_location):
         logger.error(f"The error '{e}' occurred.")
         return None
 
+
 def query_db(conn, query):
     if not query:
         logger.error('No query provided.')
@@ -52,9 +55,11 @@ def query_db(conn, query):
         cursor = conn.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
-        
+
         if not rows:
-            logger.error(f'could not retrieve any data using the following query: {os_dict["queries"][0]}.')
+            logger.error('could not retrieve any data using'
+                         'the following query: {}.'
+                         .format(query))
             return
 
         conn.close()
@@ -64,13 +69,13 @@ def query_db(conn, query):
         logger.error(f"An error occurred while querying the database: {e}")
         return None
 
+
 def retrieve_packages():
     os_dict = determine_os_database()
     connection = connect_to_db(os_dict['database location'])
     packages = query_db(connection, os_dict['queries'][0])
     return packages
 
-    
 
 def retrieve_packages_name():
     os_dict = determine_os_database()
